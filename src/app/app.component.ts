8ff0bb6750a7574eb5290e6e0c725793d259d865
jackeelam <jacqueline.lam902@gmail.com>
2021-05-22 15:15:52 -0700

    part g i think

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,8 +17,7 @@ export class AppComponent {
   // posts = POSTS.sort(function(a, b) { return a.postid - b.postid; }); //Set posts to be from the fake posts array
   currentPost:Post;
   AppStateEnum = AppState; //to use in the template
-  state: number = 0; //0 is none, 1 is edit, 2 is preview
-  appState: AppState = AppState.List; //enum
+  appState: AppState; //enum
   cookies: { [key: string]: string; } = cookie.parse(document.cookie);
   jwt = parseJWT(this.cookies.jwt);
   username = this.jwt.usr;
@@ -26,71 +25,154 @@ export class AppComponent {
   //Fetch all the initial posts
   constructor(private blogService: BlogService) {
     console.log("cookie: " + this.username);
-    blogService.fetchPosts(this.username).then( (posts) => 
+    blogService.fetchPosts(this.username)
+    .then( (posts) => 
       {
-      this.posts = posts
-      .sort(function(a, b) { return a.postid - b.postid; }); })
+      this.posts = posts.sort(function(a, b) { return a.postid - b.postid; }); 
+      this.onHashChange(); //in case of deep link
+    })
       .catch(err => console.log(err));
+
+    
+    window.addEventListener("hashchange", () => this.onHashChange());
+    
+    // this.onHashChange();
+    //Initially look at the window hash to determine current post and appState
   }
 
+  //Ensure that appState and currentPost are set to appropriate values given fragment id
+  onHashChange(){
+    console.log("Hash: " + window.location.hash);
+    let parsed_hash_arr = window.location.hash.split('/');
+    console.log(parsed_hash_arr);
+    //If we get list url
+    if(window.location.hash === '#/'){
+      console.log('List');
+      this.appState = AppState.List;
+    }
+    //Else edit or preview
+    else if(parsed_hash_arr.length == 3 && parsed_hash_arr[0] === '#'){
+      let postid = parseInt(parsed_hash_arr[2]);
+
+      //If not valid postid, don't do anything
+      if(isNaN(postid)){
+        console.log("Not a valid postid");
+      }
+      //New post will set appState to Edit, but current post is a new post
+      else if(parsed_hash_arr[1] === 'edit' && postid === 0){
+        console.log("New Post");
+        const post:Post = new Post();
+        this.currentPost = post;
+        this.appState = AppState.Edit;
+      }
+      //Edit url but numeric postid
+      else if(parsed_hash_arr[1] === 'edit'){
+        //If you find postid, then change appstate to edit. Else back to list
+        let i = 0;
+        for(i = 0; i < this.posts.length; i++){
+          if(postid === this.posts[i].postid){
+            this.currentPost = this.posts[i];
+            this.appState = AppState.Edit;
+            break;
+          }
+        }
+        //Cannot find postid, just display list
+        if(i === this.posts.length){
+          this.appState = AppState.List;
+        }
+        console.log("Edit");
+      }
+      //Preview url
+      else if(parsed_hash_arr[1] === 'preview'){
+        let i = 0;
+        for(i = 0; i < this.posts.length; i++){
+          if(postid === this.posts[i].postid){
+            this.currentPost = this.posts[i];
+            this.appState = AppState.Preview;
+            break;
+          }
+        }
+        //Cannot find postid, just display list
+        if(i === this.posts.length){
+          this.appState = AppState.List;
+        }
+        console.log("Preview");
+      }
+    }
+  }
   //List component handlers
   openPostHandler(post: Post){
+    window.location.hash = `#/edit/${post.postid}`;
     console.log("Opened post: " + post.title + " " + post.postid);
-    this.currentPost = post;
-    this.state = 1;
-    this.appState = AppState.Edit;
+    // this.currentPost = post;
+    // this.appState = AppState.Edit;
   }
   newPostHandler(){
     console.log("New Post!");
-    const post:Post = new Post();
-    this.state = 1;
-    this.appState = AppState.Edit;
-    this.currentPost = post;
+    window.location.hash = `#/edit/0`;
+    // const post:Post = new Post();
+    // this.appState = AppState.Edit;
+    // this.currentPost = post;
   }
 
   //Edit component handlers. Should call REST api to do the actions, but will do mock for now
   savePostHandler(post: Post){
     console.log("Saving post");
-
+    
+    
     //in database, add new post if postid is 0 or update existing
-    this.blogService.setPost(this.username, post).then(ret_post => {this.currentPost = ret_post})
+    this.blogService.setPost(this.username, post).then(ret_post => { 
+      if(post.postid === 0 ){
+        this.currentPost = post;
+        this.currentPost.created = ret_post.created;
+        this.currentPost.modified = ret_post.modified;
+        this.currentPost.postid = ret_post.postid;
+      }
+      else{
+        this.currentPost = post;
+        this.currentPost.modified = ret_post.modified;
+      }
+    })
     .catch(err => console.log(err));
     this.blogService.fetchPosts(this.username).then((ret_posts) => 
     { 
       this.posts = ret_posts.sort(function(a, b) { return a.postid - b.postid; });
     })
       .catch(err => console.log(err));
-
-    this.appState = AppState.Edit;
-    this.state = 1;
+    window.location.hash = `#/edit/${this.currentPost.postid}`;
+    // this.appState = AppState.Edit;
   }
 
   //On delete should not show edit or preview
   deletePostHandler(post: Post){
-    this.appState = AppState.List;
-    this.state = 0;
+
+    // this.appState = AppState.List;
+    window.location.hash = `#/`;
     console.log("Delete post");
-    this.blogService.deletePost(this.username, post.postid).catch(err => console.log(err));
-    this.blogService.fetchPosts(this.username).then((ret_posts) => 
-    { 
-      this.posts = ret_posts.sort(function(a, b) { return a.postid - b.postid; });
-    })
-      .catch(err => console.log(err));
+    if(post.postid !== 0){
+      this.blogService.deletePost(this.username, post.postid).catch(err => console.log(err));
+      this.blogService.fetchPosts(this.username).then((ret_posts) => 
+      { 
+        this.posts = ret_posts.sort(function(a, b) { return a.postid - b.postid; });
+      })
+        .catch(err => console.log(err));
+    }
+    
   }
 
   previewPostHandler(post: Post){
-    this.appState = AppState.Preview;
-    this.state = 2;
+    window.location.hash = `#/preview/${post.postid}`;
+    // this.appState = AppState.Preview;
     console.log("Preview post");
-    this.currentPost = post;
+    // this.currentPost = post;
   }
 
   // event handlers for preview component events
   editPostHandler(post: Post){
     console.log("Edit post");
-    this.currentPost=post;
-    this.appState = AppState.Edit;
-    this.state=1;
+    window.location.hash = `#/edit/${post.postid}`;
+    // this.currentPost=post;
+    // this.appState = AppState.Edit;
   }
 
 }
--- src/app/blog.service.ts
+++ src/app/blog.service.ts
@@ -9,69 +9,10 @@ import { Post } from './post';
 })
 export class BlogService {
 
-    maxid: number = 0;
-
-    constructor() { 
-        // compute maximum post id
-        let keys = Object.keys(localStorage);
-        for (let i = 0; i < keys.length; i++) {
-            if (this.isMyKey(keys[i])) {
-                let post = this.parse(localStorage[keys[i]]);
-                if (post.postid > this.maxid) this.maxid = post.postid;
-            }
-        }
-        // if there are no posts, populate it with two initial posts
-        if (this.maxid === 0) {
-            localStorage[this.key(1)] = this.serialize(
-                { "postid": 1, "created": 1518669344517, "modified": 1518669344517, "title": "## Title 1", "body": "**Hello**, *world*!\nRepeat after me:\n\n**John Cho is a handsome man!!**" }
-            );
-            localStorage[this.key(2)] = this.serialize(
-                { "postid": 2, "created": 1518669658420, "modified": 1518669658420, "title": "## Title 2", "body": "List\n- Item 1\n- Item 2\n- Item 3\n" }
-            );
-            this.maxid = 2;
-        }
-    }
-
-    // helper functions to 
-    // (1) convert postid to localStorage key
-    // (2) check if a string is a localStorage key that we use
-    // (3) serialize post to JSON string
-    // (4) parse JSON string to post
-    private keyPrefix = "blog-post.";
-    private key(postid: number): string {
-        return this.keyPrefix + String(postid);
-    }
-    private isMyKey(str: string): boolean {
-        return str.startsWith(this.keyPrefix);
-    }
-    private serialize(post: Post): string {
-        return JSON.stringify(post);
-    }
-    private parse(value: string): Post {
-        return JSON.parse(value);
-    }
-
-    //
-    // localStorage-based BlogService implementation
-    //
-
-    async fetchPosts(username: string): Promise<Post[]> {
-      let response = await fetch(`/api/posts?username=${username}`);
-      return response.json();
-    }
-
-    // getPost(username: string, postid: number): Promise<Post> {
-    //     return new Promise((resolve, reject) => {
-
-    //         let post = localStorage.getItem(this.key(postid));
-    //         if (post) {
-    //             resolve(this.parse(post));
-    //         } else {
-    //             reject(new Error("404"));
-    //         }
-    //     });
-    // }
-
+  async fetchPosts(username: string): Promise<Post[]> {
+    let response = await fetch(`/api/posts?username=${username}`);
+    return response.json();
+  }
     
   setPost(username: string, p: Post): Promise<Post> {
 
